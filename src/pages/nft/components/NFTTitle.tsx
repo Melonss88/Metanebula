c3278c6cbcb35292dc4e84db76a15d64f64105fe
Melon <chiyudong@hotbit.pro>
2025-04-03 19:37:34 +0800

    new dev

--- src/pages/nft/components/NFTTitle.tsx
+++
@@ -1,9 +0,0 @@
-const NFTTitle = ({ title }: { title: string }) => {
-  return (
-    <p className="text-[4.4rem] small:text-[2rem] w-default my-[15px]">
-      {title}
-    </p>
-  );
-};
-
-export default NFTTitle;
